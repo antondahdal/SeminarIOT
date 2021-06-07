f442ea9b24e41cc272772aad54af0996390d5a35
antondahdal <62879396+antondahdal@users.noreply.github.com>
2021-06-07 18:04:16 +0300

    adding some features

--- Seminar/src/Controller.java
+++ Seminar/src/Controller.java
@@ -1,57 +1,60 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import sun.java2d.pipe.hw.AccelGraphicsConfig;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.security.Timestamp;
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Controller implements DataObservable {
 	Location location = new Location();
 	public ArrayList<Lost> Out = new ArrayList<Lost>();
+	int numOfChapter=0;
 
-	public Controller() {
-		File PredictMetersFile = new File("Copy of all.csv");
+	public Controller(int chapterNum) {
+numOfChapter=chapterNum;
+File PredictMetersFile = new File("Copy of all.csv");
 		location.plotFromFile(PredictMetersFile);
 	}
-
+	
+	
+	public void setNumOfChapter(int numOfChapter) {
+		this.numOfChapter = numOfChapter;
+	}
+	public int getNumOfChapter() {
+		return numOfChapter;
+	}
+	
 	@Override
 	public void onDataReceived(List<EnvData> list) {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		LocalDateTime now = LocalDateTime.now();
+		
+
 		for (EnvData e : list) {
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+			LocalDateTime now = LocalDateTime.now();
+			
 			 
-//we made the temp in this example 40 and more if there's a fire and 20 and less if it's very cold
+			 if(getNumOfChapter()== 1){
 			if (e instanceof HTData) {
 				HTData x = (HTData) e;
-				if (x.getTemp() > 40) {
+				System.out.println(x.getTemp());
+				if (x.getTemp() > 70) {
 					System.out.println("The temperature Is"+x.getTemp()+"there's a Fire. Check !!");
 				
 				}
-				if (x.getTemp() <20) {
+				if (x.getTemp() <10) {
 					System.out.println("Heat Up The House It's Very Cold !!");
 				}
 			}
-			
-			
-
+			 }
+			 
+			 
+			 if(getNumOfChapter()==2){
 			if (e instanceof Ibeacon) {
 				 checkIFStillOut();
 				Ibeacon x = (Ibeacon) e;
+				System.out.println(x.getRssi());
 				Lost y = new Lost(x.getMac(), x.getTimeStamp(), x.getRssi());
 				
 				System.out.println(location.PredictedMeters(y.getRssi()));
-				
-				
-				
 				
 				if (location.PredictedMeters(y.getRssi()) >= 10) {
 					if (Out.size() == 0) {
@@ -70,26 +73,25 @@ public class Controller implements DataObservable {
 					
 				}
 
-				
 				  if(location.PredictedMeters(y.getRssi())<=9) {
 					  Out=new ArrayList<Lost>();
 						System.out.println(y.getMac()+" Is In the House");
 
 							  Out.remove(y);
 						  }
-					  
-					  
-				  
-				 
 			}
-			
+			 }
+			 if(getNumOfChapter()==3){
 			if(e instanceof AccData) {
 				AccData accData=(AccData)e;
+				System.out.println(accData.getX());
 				if(accData.SumVector()>0.3&&accData.SumVector()<0.5) {
 					System.out.println("The elderly Fell Check Him Now!!");
 				}
 			}
+			 }
 		}
+	
 	}
 
 	public void checkIFStillOut() {
@@ -109,6 +111,7 @@ public class Controller implements DataObservable {
 
 		}
 	}
+	
 
 	
 }
--- Seminar/src/TestClass.java
+++ Seminar/src/TestClass.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 
 public class TestClass {
 
 	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
 		Comminicuator.getInstance().start();
-		Controller controller=new Controller();
-
+		System.out.println("What part of The Project You Want To Run(1-3)");
+		System.out.println("the first for the Heat Detcting "+"\n"+"Second For Montring The movement  "+"\n"+"Third For Fall Detecting ");
+		int part=in.nextInt();
+		Controller controller=new Controller(part);
+System.out.println(controller.getNumOfChapter());
 		Comminicuator.getInstance().addListener(controller);
 		String topic="/gw/ac233fc02255/status";
 		int qos=0;
